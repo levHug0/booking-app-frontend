5a7328580e66bc0f91ec2a9ee108b5ff7a8f40ec
Levi Hugo <levinard.hugo@connect.qut.edu.au>
2019-04-20 12:58:50 +1000

    Landing page completed

--- app/components/landing-background.js
+++ app/components/landing-background.js
@@ -4,20 +4,18 @@ export default Component.extend({
 
     didInsertElement() {
         this._super(...arguments);
-        Ember.$('body').addClass('landing-background');
     },
 
     willDestroyElement() {
         this._super(...arguments);
-        Ember.$('body').removeClass('landing-background');
     },
 
     actions: {
         show() {
-            Ember.$.getJSON('http://localhost:1337/rooms?id=3').then( res => {
-                console.log(res);
-                console.log(res.length);
-            });
+            //Ember.$.getJSON('http://localhost:1337/rooms?id=3').then( res => {
+            //    console.log(res);
+            //    console.log(res.length);
+            //});
         }
     }
 });
