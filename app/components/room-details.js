6d6bef34597706ef9899c5185efa07c44411e50a
Levi Hugo <levinard.hugo@connect.qut.edu.au>
2019-04-21 12:17:05 +1000

    UI added for booking a room

--- app/components/room-details.js
+++ app/components/room-details.js
@@ -1,4 +1,12 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    didInsertElement() {
+        this._super(...arguments);
+        
+        this.$('.date-pickr').flatpickr({
+            dateFormat: 'd M, Y',
+            minDate: 'today'
+        });
+    }
 });
